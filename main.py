5ba20b3297fc33a92ec04dc2ca10e4477e0af3d7
Charles Marchee <80103899+charles-marchee@users.noreply.github.com>
2024-07-04 08:06:02 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import torch
+import pandas as pd
+from sklearn.model_selection import train_test_split
+import torch 
 import torch.nn as nn
 import torch.nn.functional as F
-
-import pandas as pd
 import matplotlib.pyplot as plt
 
 class Model(nn.Module):
@@ -18,10 +18,59 @@ class Model(nn.Module):
         x = self.out(x)
 
         return x 
-    
-torch.manual_seed(41)
-model = Model()
 
-# TRAINING
+######################################### PREPARE DATA ################################################################
+
 url = "https://gist.githubusercontent.com/netj/8836201/raw/6f9306ad21398ea43cba4f7d537619d0e07d5ae3/iris.csv"
 my_df = pd.read_csv(url)
+
+my_df['variety'] = my_df['variety'].replace('Setosa', 0.0)
+my_df['variety'] = my_df['variety'].replace('Versicolor', 1.0)
+my_df['variety'] = my_df['variety'].replace('Virginica', 2.0)
+
+x = my_df.drop('variety', axis=1) # we need to drop the last column named variety because that will be our y
+y = my_df['variety']
+
+# TURN FORM DATAFRAM TO NUMPY ARRAYS
+X = x.values
+Y = y.values
+
+#########################################################################################################################
+
+# USE DATA
+x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=41)
+
+x_train = torch.FloatTensor(x_train)
+x_test = torch.FloatTensor(x_test)
+
+y_train =  torch.LongTensor(y_train)
+y_test =  torch.LongTensor(y_test)
+
+criterion = nn.CrossEntropyLoss()
+
+# TRAIN MODEL
+model = Model()
+optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
+
+epochs = 100
+losses = []
+for i in range(epochs):
+    y_pred = model.forward(x_train)
+
+    loss = criterion(y_pred, y_train)
+
+    losses.append(loss.detach().numpy())
+    if i % 10 == 0:
+        print(f'Epoch: {i} loss: {loss}')
+
+optimizer.zero_grad()
+loss.backward()
+optimizer.step()
+
+# GRAPH!
+plt.plot(range(epochs), losses)
+plt.ylabel("loss/error")
+plt.xlabel("Epoch")
+
+
+
